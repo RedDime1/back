c1a1c2e03538d135cff7d57b4ac48660bf03834e
RedDime1 <dima.dima0029@gmail.com>
2025-06-08 20:13:45 +0300

    fix course id

--- src/api/course/controllers/course.ts
+++ src/api/course/controllers/course.ts
@@ -3,5 +3,23 @@
  */
 
 import { factories } from '@strapi/strapi'
+import {Context} from "koa";
 
-export default factories.createCoreController('api::course.course');
+// export default factories.createCoreController('api::course.course');
+
+export default factories.createCoreController(
+    'api::course.course',
+    ({ strapi }) => ({
+        async find(ctx) {
+            const { data, meta } = await super.find(ctx);
+
+            // Модифицируем ID в возвращаемых данных
+            const modifiedData = data.map(item => ({
+                ...item,
+                id: item.id - 1,
+                link: "./course/list.html?id=" + (item.id - 1)
+            }));
+
+            return { data: modifiedData};
+        },
+    }));
